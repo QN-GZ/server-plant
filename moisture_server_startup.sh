557345b9d0f12d8e9f6f55fb6c687e4b395914a3
Quang Nguyen <quang.nguyen@goalzero.com>
2024-04-05 15:23:19 -0600

    WIP starting react server

--- moisture_server_startup.sh
+++ moisture_server_startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 node /home/pi/projects/server-plant/index.js > /home/pi/projects/server-plant/moist_server.log 2>&1 &
 
-serve -s ~/projects/plant-sensor-react-server/build &
+serve -s /home/pi/projects/plant-sensor-react-server/build &
